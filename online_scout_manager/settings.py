6e49150bbc89ed51e049556f9bdb9176900865d5
Ryan Ford <fordryan01@hotmail.co.uk>
2026-01-06 17:43:51 +0000

    Updating the security key

--- online_scout_manager/settings.py
+++ online_scout_manager/settings.py
@@ -27,7 +27,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7hc7)ck7@o)m@g0nqzff@c!y1rpg^jb=!njh4etq2icrldteee'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
