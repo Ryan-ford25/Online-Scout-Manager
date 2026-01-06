6da9c7d04cf9e00f038e98ca2226348332062c52
Ryan Ford <fordryan01@hotmail.co.uk>
2026-01-06 17:00:20 +0000

    Adding the 'env.py' file and adding the link to the postgresql database, and installing the required packages to connect to the database

--- online_scout_manager/settings.py
+++ online_scout_manager/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7hc7)ck7@o)m@g0nqzff@c!y1rpg^jb=!njh4etq2icrldteee'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
