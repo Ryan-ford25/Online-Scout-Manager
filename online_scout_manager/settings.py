12dd301223961a90aa840df2f29931a8579bb4fb
Ryan Ford <fordryan01@hotmail.co.uk>
2026-01-06 16:39:43 +0000

    Making the first migrate and creating a superuser

--- online_scout_manager/settings.py
+++ online_scout_manager/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "add_badge",
+    "dashboard",
+    "accounts",
 ]
 
 MIDDLEWARE = [
