3ee3fde1d7cc2e4dfea411e54c7609d34134cd8a
Ryan Ford <fordryan01@hotmail.co.uk>
2026-01-06 17:17:09 +0000

    Creating a templates folder and the 'base.html' file that will be used as a foundation for all pages

--- online_scout_manager/settings.py
+++ online_scout_manager/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,12 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'allauth',
-    'allauth_account',
+    "accounts",
+    "allauth",
+    'allauth.account',
     'allauth.socialaccount',
     "add_badge",
     "dashboard",
-    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -54,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'online_scout_manager.urls'
@@ -80,13 +85,16 @@ WSGI_APPLICATION = 'online_scout_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+##DATABASES = {
+#    'default': {
+##        'ENGINE': 'django.db.backends.sqlite3',
+##       'NAME': BASE_DIR / 'db.sqlite3',
+##    }
+##}
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
