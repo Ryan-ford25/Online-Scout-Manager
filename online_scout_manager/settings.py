e4ce5cbfb5311669434a2ab89a2b49a9543b78d4
Ryan Ford <fordryan01@hotmail.co.uk>
2026-01-08 19:19:03 +0000

    Adding the 'blog' app and POST model so that users can upload any questions they have

--- online_scout_manager/settings.py
+++ online_scout_manager/settings.py
@@ -30,7 +30,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     "add_badge",
     "dashboard",
+    "blog",
 ]
 
 ACCOUNT_LOGIN_METHODS = {"username"}
