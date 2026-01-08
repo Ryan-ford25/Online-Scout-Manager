e30f3c01cf7e940f533a7030730be3a76ca73e2b
Ryan Ford <fordryan01@hotmail.co.uk>
2026-01-08 18:30:41 +0000

    Adjusting the view fo he dashboard so that the badges that have been earned are listed under 'your earned badges'

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,16 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from add_badge.models import Badge, BadgeRequest
+from add_badge.models import ScoutBadge, BadgeRequest, Badge
 
 @login_required
 def scout_dashboard(request):
-    return render(request, 'dashboard/scout_dashboard.html')
-
-
-@login_required
-def leader_dashboard(request):
-    return render(request, 'dashboard/leader_dashboard.html')
-
+    earned_badges = ScoutBadge.objects.filter( scout=request.user).select_related("badge")
+    return render(request, 'dashboard/scout_dashboard.html', {'earned_badges': earned_badges})
 
 @login_required
 def home(request):
