dbb17b4fa0f8a47c6a3ac9c3bd57d63784d96431
Ryan Ford <fordryan01@hotmail.co.uk>
2026-01-09 18:15:00 +0000

    Created a static image file and added the memebership badge as a default image to be loaded wih the blog posts for now

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
@@ -8,4 +8,28 @@ from .models import Post
 class PostList(generic.ListView):
     queryset = Post.objects.all()
     template = "post_list.html"
+    post_detail(request, slug)
     paginate_by = 6
+
+def post_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )
