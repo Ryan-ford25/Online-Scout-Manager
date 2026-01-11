cbf5a53f610cf2671003764d14e90745b05f3857
Ryan Ford <fordryan01@hotmail.co.uk>
2026-01-11 08:46:35 +0000

    Adding the functionality to delete or edit previously written comments

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('blog/', views.PostList.as_view(), name = 'blog'),
     path('posts/<slug:slug>/', views.post_detail, name="post_detail"),
+    path('posts/<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name='comment_edit'),
+    path('posts/<slug:slug>/delete_comment/<int:comment_id>', views.comment_delete, name='comment_delete'),
 ]
