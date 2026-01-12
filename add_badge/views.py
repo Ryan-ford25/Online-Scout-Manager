314d07574db37acf9392280639e04a329ab6e3a5
Ryan Ford <fordryan01@hotmail.co.uk>
2026-01-12 16:45:26 +0000

    Adding in the request badge button for users with a scout role only it will not appear for leaders

--- add_badge/views.py
+++ add_badge/views.py
@@ -3,6 +3,7 @@ from django.views import generic
 from .models import Badge, BadgeRequest, BadgeRequirements, ScoutBadge
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib import messages
 # Create your views here.
 
 class BadgeList(generic.ListView):
@@ -18,7 +19,9 @@ def request_badge(request, badge_id):
         scout=request.user,
         badge=badge
     )
-
+    messages.add_message(
+        request, messages.SUCCESS, 'Badge request submitted.'
+        )
     return redirect("dashboard:scout")
 
 @login_required
@@ -37,12 +40,31 @@ def reject_badge_request(request, request_id):
 
 def badge_detail(request, slug):
     badge = get_object_or_404(Badge, slug=slug)
-    requirement = badge.requirements.all()
+    requirements = badge.requirements.all()
+
+    has_badge = False
+    has_pending_request = False
+
+    if request.user.is_authenticated:
+        has_badge = ScoutBadge.objects.filter( 
+            scout=request.user,
+            badge=badge
+        ).exists()
+        
+        has_pending_request = BadgeRequest.objects.filter(
+            scout=request.user,
+            badge=badge,
+            status="pending"
+        ).exists()
+
+        
     return render(
         request,
         "add_badge/badge_detail.html",
         {
             "badge": badge,
-            "requirement":requirement,
+            "requirements":requirements,
+            'has_badge': has_badge,
+            "has_pending_request": has_pending_request,
         },
     )
