eded1afbfdcc69029f2a97849225f61383474029
Ryan Ford <fordryan01@hotmail.co.uk>
2026-01-24 19:18:10 +0000

    Editing the urls to remove a 'trailing slash' html validator flagged it as not an error just helpful information

--- add_badge/urls.py
+++ add_badge/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("reject/<int:request_id>/", views.reject_badge_request, name="reject_badge_request"),
     path("badge/<slug:slug>/", views.badge_detail, name = "badge_detail"),
     path("badges/add/", views.add_badge, name = "add_badge"),
-    path('badges/<int:badge_id>/edit/', views.edit_badge, name='edit_badge'),
+    path('badges/<int:badge_id>/edit', views.edit_badge, name='edit_badge'),
     path("badges/delete/<int:badge_id>/", views.delete_badge, name="delete_badge"),
     path("featured_badge/<int:scout_badge_id>/", views.featured_badges,name="featured_badge" ),
 ]
