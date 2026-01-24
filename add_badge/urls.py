16ef7bdc8ceef7e26cb0f75a51458d08f2eac5c7
Ryan Ford <fordryan01@hotmail.co.uk>
2026-01-24 19:35:04 +0000

    Removing the trailing slash from the login.html

--- add_badge/urls.py
+++ add_badge/urls.py
@@ -3,12 +3,12 @@ from django.urls import path
 
 urlpatterns = [
     path('badges/', views.BadgeList.as_view(), name = 'badges'),
-    path("request/<int:badge_id>/", views.request_badge, name="request_badge"),
-    path("approve/<int:request_id>/", views.approve_badge_request, name="approve_badge_request"),
-    path("reject/<int:request_id>/", views.reject_badge_request, name="reject_badge_request"),
-    path("badge/<slug:slug>/", views.badge_detail, name = "badge_detail"),
-    path("badges/add/", views.add_badge, name = "add_badge"),
+    path("request/<int:badge_id>", views.request_badge, name="request_badge"),
+    path("approve/<int:request_id>", views.approve_badge_request, name="approve_badge_request"),
+    path("reject/<int:request_id>", views.reject_badge_request, name="reject_badge_request"),
+    path("badge/<slug:slug>", views.badge_detail, name = "badge_detail"),
+    path("badges/add", views.add_badge, name = "add_badge"),
     path('badges/<int:badge_id>/edit', views.edit_badge, name='edit_badge'),
-    path("badges/delete/<int:badge_id>/", views.delete_badge, name="delete_badge"),
-    path("featured_badge/<int:scout_badge_id>/", views.featured_badges,name="featured_badge" ),
+    path("badges/delete/<int:badge_id>", views.delete_badge, name="delete_badge"),
+    path("featured_badge/<int:scout_badge_id>", views.featured_badges,name="featured_badge" ),
 ]
