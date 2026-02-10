be3b75c2b2bb2b20dffdd5ec5cc619e218130e21
Ryan Ford <71533497+Ryan-ford25@users.noreply.github.com>
2026-02-10 15:32:55 +0000

    Delete env.py

--- env.py
+++
@@ -1,9 +0,0 @@
-import os
-
-os.environ.setdefault(
-    "DATABASE_URL", "postgresql://neondb_owner:npg_PJTKuy87xqHM@ep-odd-bar-ag06oh93.c-2.eu-central-1.aws.neon.tech/canon_busy_punk_557845"
-)
-
-os.environ.setdefault("SECRET_KEY", "874649d7afcd7b33c1912aee93bd55a9")
-
-os.environ.setdefault( "CLOUDINARY_URL", "cloudinary://198961929335267:Bj5rqWE4EzYHfx0bafMO-isFGX4@dv4rajgx6")
